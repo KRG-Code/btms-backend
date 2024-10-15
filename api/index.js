21b356a65e89abcad23ebe2f533433d8ac0a1f26
Kim Rovil Garcia <113615969+KRG-Code@users.noreply.github.com>
2024-10-15 10:47:20 +0800

    delete vercel files

--- api/index.js
+++
@@ -1,9 +0,0 @@
-const app = require('../server'); // Adjust the path according to your folder structure
-
-module.exports = (req, res) => {
-  // Use the express app
-  app(req, res);
-};
-
-
-//ssss
